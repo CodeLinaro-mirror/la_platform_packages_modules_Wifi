3d2577df52f2d19bbf1d60fed61b923d13c9003c
Xin Deng <quic_deng@quicinc.com>
2022-05-09 15:40:11 +0800

    Call handleNetworkDisconnect before trasition to disconnected state
    ClientModeImpl starts ignoring disconnect messages once it's
    transitioned to disconnected state. Therefore need to make sure that we
    always call handleNetworkDisconnect before the transition to clean up
    internal states and to sendout the disconnect broadcast.
    
    Bug: 195755234
    Bug: 195574603
    Test: atest com.android.server.wifi
    Test: Manually verified that the device no longer gets stuck on
    "authenticating..." when connecting with the wrong password.
    
    CRs-Fixed: 3162682
    Change-Id: I7f106dcf857b65c8d044daab830e623a29041104

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -4532,6 +4532,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                                 WifiMetrics.ConnectionEvent.FAILURE_NETWORK_NOT_FOUND,
                                 WifiMetricsProto.ConnectionEvent.HLF_NONE,
                                 WifiMetricsProto.ConnectionEvent.FAILURE_REASON_UNKNOWN);
+                        handleNetworkDisconnect(false,
+                                WifiStatsLog.WIFI_DISCONNECT_REPORTED__FAILURE_CODE__UNSPECIFIED);
                         transitionTo(mDisconnectedState); // End of connection attempt.
                     }
                     break;
@@ -4583,6 +4585,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                                 WifiLastResortWatchdog.FAILURE_CODE_ASSOCIATION,
                                 isConnected());
                     }
+                    handleNetworkDisconnect(false,
+                            WifiStatsLog.WIFI_DISCONNECT_REPORTED__FAILURE_CODE__UNSPECIFIED);
                     transitionTo(mDisconnectedState); // End of connection attempt.
                     break;
                 }
@@ -4662,6 +4666,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                                 WifiLastResortWatchdog.FAILURE_CODE_AUTHENTICATION,
                                 isConnected());
                     }
+                    handleNetworkDisconnect(false,
+                            WifiStatsLog.WIFI_DISCONNECT_REPORTED__FAILURE_CODE__UNSPECIFIED);
                     transitionTo(mDisconnectedState); // End of connection attempt.
                     break;
                 }

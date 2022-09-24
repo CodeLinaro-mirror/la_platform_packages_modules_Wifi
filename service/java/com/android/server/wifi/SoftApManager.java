9eda9dc84bdc518aafdc7c5c2fb38e8c0e7416f1
Rongjing Liao <quic_liaor@quicinc.com>
2022-09-24 18:24:32 +0800

    wifi: mapping primary/secodnary LOHS to 2nd wlan chip
    Per OEM requirment, mapping primary and secodnary LOHS to
    secondary wlan chip, tethered AP to primary wlan chip.
    
    Change-Id: Iad093b878899bce585be580bf66b669d85f1a0f4
    CRs-Fixed: 3278210

--- service/java/com/android/server/wifi/SoftApManager.java
+++ service/java/com/android/server/wifi/SoftApManager.java
@@ -17,6 +17,8 @@
 package com.android.server.wifi;
 
 import static android.net.wifi.WifiManager.SAP_CLIENT_DISCONNECT_REASON_CODE_UNSPECIFIED;
+import static android.net.wifi.WifiManager.LOCAL_ONLY_HOTSPOT_TYPE_PRIMARY;
+import static android.net.wifi.WifiManager.LOCAL_ONLY_HOTSPOT_TYPE_SECONDARY;
 
 import static com.android.server.wifi.util.ApConfigUtil.ERROR_GENERIC;
 import static com.android.server.wifi.util.ApConfigUtil.ERROR_NO_CHANNEL;
@@ -43,6 +45,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.WorkSource;
+import android.sysprop.WifiProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -461,6 +464,33 @@ public class SoftApManager implements ActiveModeManager {
         return new SoftApModeConfiguration(mOriginalModeConfiguration.getTargetMode(),
                 mCurrentSoftApConfiguration, mCurrentSoftApCapability,
                 mOriginalModeConfiguration.getLohsType());
+    }
+
+    /**
+     * set property 'wifi.softap.iface.on.dual.wlan' to below value by Lohs type :
+     * below value bring two kinds of information: pre-defined AP interface name and
+     * information about AP created on primary or secondary wlan chip
+     *
+     * a)"1stIfaceOnSecondary" means AP created on secondary wlan chip, it has
+     * specified interface name stored in property 'ro.vendor.wlan.secondary.sap.1stiface'
+     *
+     * b)"2ndIfaceOnSecondary" means AP created on secondary wlan chip, it has
+     * specified interface name stored in property 'ro.vendor.wlan.secondary.sap.2ndiface'
+     *
+     * c)"ApOnPrimary" means AP created on primary wlan chip,it has specifed interface name
+     * stored in property 'ro.vendor.wlan.secondary.sap.1stiface' and
+     * 'ro.vendor.wlan.secondary.sap.2ndiface'for bridged AP
+     *
+     * Note: the first installed wlan driver module is for primary wlan chip, the later one
+     * is for secondary wlan chip.
+     */
+    private void setSoftApIfaceOnSecondWlan() {
+        if (mOriginalModeConfiguration.getLohsType() == LOCAL_ONLY_HOTSPOT_TYPE_PRIMARY)
+            WifiProperties.softap_iface_on_dual_wlan("1stIfaceOnSecondary");
+        else if (mOriginalModeConfiguration.getLohsType() == LOCAL_ONLY_HOTSPOT_TYPE_SECONDARY)
+            WifiProperties.softap_iface_on_dual_wlan("2ndIfaceOnSecondary");
+        else
+            WifiProperties.softap_iface_on_dual_wlan("ApOnPrimary");
     }
 
     /**
@@ -963,6 +993,8 @@ public class SoftApManager implements ActiveModeManager {
                                         .build();
                             }
                         }
+                        setSoftApIfaceOnSecondWlan();
+                        Log.d(getTag(), "wifi.softap.iface.on.dual.wlan set to " + WifiProperties.softap_iface_on_dual_wlan());
                         mApInterfaceName = mWifiNative.setupInterfaceForSoftApMode(
                                 mWifiNativeInterfaceCallback, mRequestorWs,
                                 mCurrentSoftApConfiguration.getBand(), isBridgedMode());

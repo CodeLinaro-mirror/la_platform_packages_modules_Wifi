6588e3e911165182f3fcae5f77f671080447714e
Rongjing Liao <quic_liaor@quicinc.com>
2022-10-14 11:31:12 +0800

    Wifi: support dual LOHS in framework
    Define below local only hotspot type to support dual LOHS
    
    1) LOCAL_ONLY_HOTSPOT_TYPE_PRIMARY.
    
    2) LOCAL_ONLY_HOTSPOT_TYPE_SECONDARY.
    
    OEM App need to invoke WifiManager.setLohsConfiguration
    (int lohsType,SoftApConfiguratin config) to speficy LOHS
    type before starting dual LOHS.
    
    Change-Id: I15c91a621396b504738fc5ead134f7b1fcc7165b
    CRs-Fixed: 3278210
    (cherry picked from commit 0e360b41c80ffe658984fb5eba3390e8045e1ff4)

--- service/java/com/android/server/wifi/SoftApManager.java
+++ service/java/com/android/server/wifi/SoftApManager.java
@@ -379,7 +379,8 @@ public class SoftApManager implements ActiveModeManager {
         }
         // Store mode configuration before update the configuration.
         mOriginalModeConfiguration = new SoftApModeConfiguration(apConfig.getTargetMode(),
-                mCurrentSoftApConfiguration, mCurrentSoftApCapability);
+                mCurrentSoftApConfiguration, mCurrentSoftApCapability,
+                apConfig.getLohsType());
         if (mCurrentSoftApConfiguration != null) {
             mIsUnsetBssid = mCurrentSoftApConfiguration.getBssid() == null;
             if (mCurrentSoftApCapability.areFeaturesSupported(
@@ -535,7 +536,8 @@ public class SoftApManager implements ActiveModeManager {
      */
     public SoftApModeConfiguration getSoftApModeConfiguration() {
         return new SoftApModeConfiguration(mOriginalModeConfiguration.getTargetMode(),
-                mCurrentSoftApConfiguration, mCurrentSoftApCapability);
+                mCurrentSoftApConfiguration, mCurrentSoftApCapability,
+                mOriginalModeConfiguration.getLohsType());
     }
 
     /**
@@ -622,6 +624,7 @@ public class SoftApManager implements ActiveModeManager {
      */
     private void updateApState(int newState, int currentState, int reason) {
         mSoftApCallback.onStateChanged(newState, reason);
+        mSoftApCallback.onStateChangedWithIface(newState, reason, mApInterfaceName);
 
         //send the AP state change broadcast
         final Intent intent = new Intent(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);

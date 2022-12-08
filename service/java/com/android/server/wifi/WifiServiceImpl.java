62786c17942ba852e512d9cc8875426494973572
Rongjing Liao <quic_liaor@quicinc.com>
2022-12-08 19:34:51 +0800

    Wifi: add vendor propety to enable 6GHz band support
    To make one image support multi wlan chip with different
    capability, like qca6490 support 6GHz band while qca6390
    doesn't.
    
    Add vendor property to indicate support 6GHz band per
    wlan chip.
    
    Change-Id: I049c8ec7ce70e90a7e6f4e87c078eef595c051bd

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -123,6 +123,7 @@ import android.os.UserManager;
 import android.os.WorkSource;
 import android.os.RemoteCallbackList;
 import android.os.connectivity.WifiActivityEnergyInfo;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.telephony.CarrierConfigManager;
 import android.telephony.PhoneStateListener;
@@ -3731,7 +3732,8 @@ public class WifiServiceImpl extends BaseWifiService {
     }
 
     private boolean is6GhzBandSupportedInternal() {
-        if (mContext.getResources().getBoolean(R.bool.config_wifi6ghzSupport)) {
+        if (mContext.getResources().getBoolean(R.bool.config_wifi6ghzSupport)
+            && SystemProperties.getBoolean("ro.vendor.wlan.6ghz", false)) {
             return true;
         }
         return mWifiThreadRunner.call(
--- service/java/com/android/server/wifi/util/ApConfigUtil.java
+++ service/java/com/android/server/wifi/util/ApConfigUtil.java
@@ -18,6 +18,7 @@ package com.android.server.wifi.util;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.os.SystemProperties;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.wifi.CoexUnsafeChannel;
@@ -818,7 +819,9 @@ public class ApConfigUtil {
     public static boolean isSoftAp6GhzSupported(@NonNull Context context) {
         return context.getResources().getBoolean(R.bool.config_wifi6ghzSupport)
                 && context.getResources().getBoolean(
-                R.bool.config_wifiSoftap6ghzSupported);
+                R.bool.config_wifiSoftap6ghzSupported)
+                && SystemProperties.getBoolean(
+                "ro.vendor.wlan.6ghz", false);
     }
 
     /**

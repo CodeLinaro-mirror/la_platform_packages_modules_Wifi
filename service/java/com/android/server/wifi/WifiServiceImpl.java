4e8ddbdd46356604cb44f00aca6fcfe97af719ca
Rongjing Liao <quic_liaor@quicinc.com>
2022-12-08 19:34:51 +0800

    Wifi: add vendor propety to enable 6GHz band support
    To make one image support multi wlan chip with different
    capability, like qca6490 support 6GHz band while qca6390
    doesn't.
    
    Add vendor property to indicate support 6GHz band per
    wlan chip.
    
    CRs-Fixed: 3384978
    Change-Id: I049c8ec7ce70e90a7e6f4e87c078eef595c051bd
    (cherry picked from commit 0afe985b161ad9936b6dc44a65df23e2515e156c)

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -185,6 +185,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.WorkSource;
 import android.os.connectivity.WifiActivityEnergyInfo;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.telephony.CarrierConfigManager;
 import android.telephony.PhoneStateListener;
@@ -4965,7 +4966,8 @@ public class WifiServiceImpl extends BaseWifiService {
     }
 
     private boolean is6GhzBandSupportedInternal() {
-        if (mContext.getResources().getBoolean(R.bool.config_wifi6ghzSupport)) {
+        if (mContext.getResources().getBoolean(R.bool.config_wifi6ghzSupport)
+            && SystemProperties.getBoolean("ro.vendor.wlan.6ghz", false)) {
             return true;
         }
         return mActiveModeWarden.isBandSupportedForSta(WifiScanner.WIFI_BAND_6_GHZ);
--- service/java/com/android/server/wifi/util/ApConfigUtil.java
+++ service/java/com/android/server/wifi/util/ApConfigUtil.java
@@ -34,6 +34,7 @@ import static com.android.server.wifi.HalDeviceManager.HDM_CREATE_IFACE_STA;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.os.SystemProperties;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.wifi.CoexUnsafeChannel;
@@ -1234,7 +1235,9 @@ public class ApConfigUtil {
             case SoftApConfiguration.BAND_6GHZ:
                 return context.getResources().getBoolean(R.bool.config_wifi6ghzSupport)
                         && context.getResources().getBoolean(
-                        R.bool.config_wifiSoftap6ghzSupported);
+                        R.bool.config_wifiSoftap6ghzSupported)
+                        && SystemProperties.getBoolean(
+                        "ro.vendor.wlan.6ghz", false);
             case SoftApConfiguration.BAND_60GHZ:
                 return context.getResources().getBoolean(R.bool.config_wifi60ghzSupport)
                         && context.getResources().getBoolean(

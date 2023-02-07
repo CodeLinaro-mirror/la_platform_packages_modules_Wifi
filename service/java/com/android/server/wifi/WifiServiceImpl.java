e72f1807b8d20d6608a7511076c58332a20b783b
Nate(Qiang) Jiang <qiangjiang@google.com>
2023-02-07 01:52:58 +0000

    Revert "Revert "[DO NOT MERGE] wifi: remove certificates for network factory reset""
    Bug: 231985227
    This reverts commit 3b323b940400fa6ee89abddafb5a8a540463c2bc.
    
    Reason for revert: re-release security patch with fix
    
    Change-Id: Ie2a1de820e687a60f2246a21b5d6e603547a212c
    (cherry picked from commit dbdb6bad10bc6684676db6b2a327f7f48fc3f4c2)
    Merged-In: Ie2a1de820e687a60f2246a21b5d6e603547a212c
    (cherry picked from commit b57842633f9f9404710d5a1622618bb385e3192c)

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -4326,7 +4326,7 @@ public class WifiServiceImpl extends BaseWifiService {
                 if (network.isEnterprise()) {
                     mWifiInjector.getWifiKeyStore().removeKeys(network.enterpriseConfig, true);
                 }
-                removeNetwork(network.networkId, packageName);
+                mWifiConfigManager.removeNetwork(network.networkId, Binder.getCallingUid(), packageName);
             }
         });
         // Delete all Passpoint configurations

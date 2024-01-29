5d63ca4bc6c894ae1c894094dba5499167fc31ef
Chenming Huang <quic_chenhuan@quicinc.com>
2024-01-29 15:27:19 +0530

    Fix license markings
    Fix license markings.
    
    CRs-Fixed: 3718159
    Change-Id: Ib7a60299a7084461aa81db0719346c353a599964
    (cherry picked from commit d11d30e6d09de5b776d60c11a123de9daf62c497)

--- framework/java/android/net/wifi/BaseWifiService.java
+++ framework/java/android/net/wifi/BaseWifiService.java
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+/**
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 package android.net.wifi;
 
 import android.annotation.NonNull;

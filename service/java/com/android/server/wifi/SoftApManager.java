b8390d77db2ffe3c8ea44704a50614436911d725
Xin Deng <quic_deng@quicinc.com>
2024-04-03 18:23:24 +0800

    SAP: Add if require to start MLO SAP when start SAP
    When start SAP, add a new check before bridge mode check. If MLO is
    enabled and band numbers greater than 1 in softApConfiguration, start
    MLO SAP.
    
    CRs-Fixed: 3976518
    Change-Id: I749fb83f0bee1b3d44d8d7e06efcd887ecd74c8b

--- service/java/com/android/server/wifi/SoftApManager.java
+++ service/java/com/android/server/wifi/SoftApManager.java
@@ -537,13 +537,19 @@ public class SoftApManager implements ActiveModeManager {
                         == SoftApConfiguration.SECURITY_TYPE_WPA3_OWE_TRANSITION);
     }
 
+    private boolean isMultiLinkOperationMode() {
+        return (SdkLevel.isAtLeastT() && mCurrentSoftApConfiguration != null
+                && (mCurrentSoftApConfiguration.getBands().length > 1)
+                && (mCurrentSoftApConfiguration.isMultiLinkOperationEnabled()));
+    }
+
     private boolean isBridgedMode() {
         return (SdkLevel.isAtLeastS() && mCurrentSoftApConfiguration != null
                 && (mCurrentSoftApConfiguration.getBands().length > 1));
     }
 
     private boolean isBridgeRequired() {
-        return isBridgedMode() || isOweTransition();
+        return (isBridgedMode() || isOweTransition()) && !mCurrentSoftApConfiguration.isMultiLinkOperationEnabled();
     }
 
     private long getShutdownTimeoutMillis() {
@@ -1229,8 +1235,33 @@ public class SoftApManager implements ActiveModeManager {
                                     + ", base country in SoftApCapability = "
                                     + mCurrentSoftApCapability.getCountryCode());
                         }
-                        if (isBridgedMode()) {
-                            boolean isFallbackToSingleAp = false;
+
+                        boolean isFallbackToSingleAp = false;
+                        if (isMultiLinkOperationMode()) {
+                            if (mWifiNative.isSoftApInstanceDiedHandlerSupported()
+                                    && !TextUtils.equals(mCountryCode,
+                                      mCurrentSoftApCapability.getCountryCode())) {
+                                Log.i(getTag(), "CountryCode changed, bypass the supported band"
+                                        + "capability check, mCountryCode = " + mCountryCode
+                                        + ", base country in SoftApCapability = "
+                                        + mCurrentSoftApCapability.getCountryCode());
+                            } else {
+                                SoftApConfiguration tempConfig =
+                                        ApConfigUtil.removeUnavailableBandsFromConfig(
+                                                mCurrentSoftApConfiguration,
+                                                mCurrentSoftApCapability, mCoexManager, mContext);
+                                if (tempConfig == null) {
+                                    handleStartSoftApFailure(START_RESULT_FAILURE_UNSUPPORTED_CONFIG);
+                                    break;
+                                }
+                                mCurrentSoftApConfiguration = tempConfig;
+                                if (mCurrentSoftApConfiguration.getBands().length == 1) {
+                                    isFallbackToSingleAp = true;
+                                    Log.i(getTag(), "Removed unavailable bands"
+                                            + " - fallback to single AP");
+                                }
+                            }
+                        } else if (isBridgedMode()) {
                             final List<ClientModeManager> cmms =
                                     mActiveModeWarden.getClientModeManagers();
                             // Checking STA status only when device supports STA + AP concurrency
